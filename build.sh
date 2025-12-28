8d2a25e413d4a637f75ba89ab473ddf142d8fe0c
MauriceElliott <mauricemelliott@gmail.com>
2025-12-28 20:13:13 +0000

    fixup! fixup! fixup! Working build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-export PLAYDATE_SDK_PATH="/home/maurice/PlaydateSDK"
+export PLAYDATE_SDK_PATH="$HOME/PlaydateSDK"
 export NAME="GUTWOUND"
 export OUT_DIR="./build/$NAME"
 
@@ -8,9 +8,11 @@ export OUT_DIR="./build/$NAME"
 if [ -e ./build ]; then
   rm -d -r ./build
 fi
-
 mkdir build
 
-pdc ./source/main.lua $OUT_DIR
+#Compile from main.lua
+pdc -I $PLAYDATE_SDK_PATH ./source/main.lua $OUT_DIR
 
-PlaydateSimulator $OUT_DIR/$NAME.pdx/
+# So I don't have to clean up the pointless folders the simulator creates I cd into the build folder that'll get deleted anyways.
+cd build
+PlaydateSimulator ./$NAME.pdx/

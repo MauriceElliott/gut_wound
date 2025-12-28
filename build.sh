d30ccae2098c62f3adb660e27479a0ddc7855612
MauriceElliott <mauricemelliott@gmail.com>
2025-12-28 22:19:33 +0000

    made the build work on linux and macos

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
-export PLAYDATE_SDK_PATH="$HOME/PlaydateSDK"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export PLAYDATE_SDK_PATH="$HOME/Developer/PlaydateSDK"
+else
+  export PLAYDATE_SDK_PATH="$HOME/PlaydateSDK"
+fi
 export NAME="GUTWOUND"
 export OUT_DIR="./build/$NAME"
 
@@ -15,4 +19,8 @@ pdc -I $PLAYDATE_SDK_PATH ./source/main.lua $OUT_DIR
 
 # So I don't have to clean up the pointless folders the simulator creates I cd into the build folder that'll get deleted anyways.
 cd build
-PlaydateSimulator ./$NAME.pdx/
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  open ./$NAME.pdx/
+else
+  PlaydateSimulator ./$NAME.pdx/
+fi

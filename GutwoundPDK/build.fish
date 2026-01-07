4b31ecdfc7b2b3047071280cd2ece468392aa7c8
MauriceElliott <mauricemelliott@gmail.com>
2026-01-07 10:42:48 +0000

    Fixed build completely for linux, just need to get the simulator running again as I seem to have pruned one of its dependencies.

--- GutwoundPDK/build.fish
+++ GutwoundPDK/build.fish
@@ -4,14 +4,17 @@
 set -x ARM_NONE_EABI_GCC_PATH /usr/lib/gcc/arm-none-eabi/15
 set -x ARM_NONE_EABI_SYSROOT_PATH /usr/arm-none-eabi
 
+# Set product name
+set PRODUCT_NAME Gutwound
+
 function cleanup
     pkill -f PlaydateSimulator 2>/dev/null
 end
 
 trap cleanup EXIT
 
-cd ..
-swift package pdc
+# Build the package, filtering out pointless library version warnings
+swift package pdc 2>&1 | grep -v "libtinfo.so.6: no version information" | grep -v "libxml2.so.2: no version information" | grep -v "libcurl.so.4: no version information"
 
 # Create a symbolic link to the compiled pdx in the Playdate Simulator Games dir
 # This allows browsing the pdx as a sideloaded game, helpful for checking icons
@@ -20,4 +23,5 @@ if test -L $PLAYDATE_SDK_PATH/Disk/Games/$PRODUCT_NAME.pdx
 end
 ln -s (pwd)/.build/plugins/PDCPlugin/outputs/$PRODUCT_NAME.pdx $PLAYDATE_SDK_PATH/Disk/Games/$PRODUCT_NAME.pdx
 
+# Launch the simulator (note: may need webkit2gtk-4.1 library installed on Linux)
 $PLAYDATE_SDK_PATH/bin/PlaydateSimulator $PLAYDATE_SDK_PATH/Disk/Games/$PRODUCT_NAME.pdx

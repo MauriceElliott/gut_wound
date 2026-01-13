e7e4010aecf0e0ac2e6ec80bdaf64e7a0ee547e6
MauriceElliott <maurice.elliott@telusagcg.com>
2026-01-13 12:09:12 +0000

    fixup! fixup! testing sprites/tilesets out with the build

--- Sources/Gutwound/Game.swift
+++ Sources/Gutwound/Game.swift
@@ -1,44 +1,41 @@
 import PlaydateKit
 
-// MARK: - Game
+enum Scene {
+    case mainLoop
+    case paused
+    case startMenu
+}
 
 final class Game: PlaydateGame {
-    // MARK: Lifecycle
-
+    let gameScene: Scene = .startMenu
     init() {
         room.addToDisplayList()
     }
-
-    // MARK: Internal
 
     let room = Room()
 
     func update() -> Bool {
         Sprite.updateAndDrawDisplayListSprites()
-        System.drawFPS()
         return true
     }
 
     func gameWillPause() {
-        print("Paused!")
+        
     }
 }
 
-// MARK: - Logo
-
 class Room: Sprite.Sprite {
-    // let collisionMap: Graphics.Bitmap
-    // let flagsMap: Graphics.Bitmap
+    let collisionMap: Graphics.Bitmap
+    let flagsMap: Graphics.Bitmap
     override init() {
         super.init()
         image = try! Graphics.Bitmap(path: "test_room.png")
-        // collisionMap = try! Graphics.Bitmap(path: "test_room_collission_map.png")
-        // flagsMap = try! Graphics.Bitmap(path: "test_room_flags_map.png")
+        collisionMap = try! Graphics.Bitmap(path: "test_room_collission_map.png")
+        flagsMap = try! Graphics.Bitmap(path: "test_room_flags_map.png")
         bounds = .init(x: 0, y: 0, width: 400, height: 240)
     }
 
-    // MARK: Internal
-
     override func update() {
+
     }
 }

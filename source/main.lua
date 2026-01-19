7c5c986c835ad49a81cb4b1048e0ee194504fa38
MauriceElliott <mauricemelliott@gmail.com>
2026-01-19 21:43:31 +0000

    Added extremely basic character movement just cus

--- source/main.lua
+++ source/main.lua
@@ -12,13 +12,27 @@ local manImage = gfx.image.new("assets/man")
 
 local roomSprite = gfx.sprite.new(roomImage)
 local manSprite = gfx.sprite.new(manImage)
+local manX = 200
+local manY = 120
 
 roomSprite:add()
 roomSprite:moveTo(200, 120)
 
 manSprite:add()
-manSprite:moveTo(200, 120)
+manSprite:moveTo(manX, manY)
 
 function pd.update()
     gfx.sprite.update()
+
+    if pd.buttonIsPressed(pd.kButtonUp) then
+        manY -= 1
+    elseif pd.buttonIsPressed(pd.kButtonDown) then
+        manY += 1
+    elseif pd.buttonIsPressed(pd.kButtonLeft) then
+        manX -= 1
+    elseif pd.buttonIsPressed(pd.kButtonRight) then
+        manX += 1
+    end
+
+    manSprite:moveTo(manX, manY)
 end

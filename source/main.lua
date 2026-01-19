d6d0adb274b12e90403198d46ab332ddc265cfb9
MauriceElliott <mauricemelliott@gmail.com>
2026-01-19 11:57:01 +0000

    added man

--- source/main.lua
+++ source/main.lua
@@ -8,11 +8,16 @@ import "CoreLibs/sprites"
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 local roomImage = gfx.image.new("assets/test_room")
-print(roomImage)
+local manImage = gfx.image.new("assets/man")
+
 local roomSprite = gfx.sprite.new(roomImage)
+local manSprite = gfx.sprite.new(manImage)
 
 roomSprite:add()
 roomSprite:moveTo(200, 120)
+
+manSprite:add()
+manSprite:moveTo(200, 120)
 
 function pd.update()
     gfx.sprite.update()

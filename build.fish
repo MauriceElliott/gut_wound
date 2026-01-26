ce60ed1197bb1ea192d83a5a7af69c7cf87881c9
MauriceElliott <mauricemelliott@gmail.com>
2026-01-26 22:12:11 +0000

    fixup! Finished off switch back

--- build.fish
+++ build.fish
@@ -1,38 +1,19 @@
 #!/usr/bin/env fish
 
-# Set product name
+# Fixes an issue for Linux, or possibly specifically my gentoo install where when compiling, swift can't find my studio.h file.
+set -x ARM_NONE_EABI_GCC_PATH /usr/lib/gcc/arm-none-eabi/15
+set -x ARM_NONE_EABI_SYSROOT_PATH /usr/arm-none-eabi
+
 set PRODUCT_NAME Gutwound
+set GAME_PATH $PLAYDATE_SDK_PATH/Disk/Games/$PRODUCT_NAME.pdx
 
-# Check if PLAYDATE_SDK_PATH is set
-if not set -q PLAYDATE_SDK_PATH
-    echo "Error: PLAYDATE_SDK_PATH environment variable is not set"
-    echo "Please set it to your Playdate SDK installation directory"
-    exit 1
+swift package pdc 2>&1 | grep -v "libtinfo.so.6: no version information" | grep -v "libxml2.so.2: no version information" | grep -v "libcurl.so.4: no version information"
+
+if test -L $GAME_PATH
+    rm -rf $GAME_PATH
 end
+ln -s (pwd)/.build/plugins/PDCPlugin/outputs/$PRODUCT_NAME.pdx $GAME_PATH
 
-if not test -d ./.builds
-    mkdir .builds
-end
+cd .build
 
-if not test -L ./compile_commands.json
-    xmake project -k compile_commands
-end
-
-echo "Building $PRODUCT_NAME.pdx..."
-xmake build
-
-if test $status -ne 0
-    echo "Build failed!"
-    exit 1
-end
-
-echo "Build successful!"
-
-if test -L $PLAYDATE_SDK_PATH/Disk/Games/$PRODUCT_NAME.pdx
-    rm -rf $PLAYDATE_SDK_PATH/Disk/Games/$PRODUCT_NAME.pdx
-end
-ln -s (pwd)/.builds/$PRODUCT_NAME.pdx $PLAYDATE_SDK_PATH/Disk/Games/$PRODUCT_NAME.pdx
-
-cd .builds
-
-$PDSIM ./$PRODUCT_NAME.pdx
+$PDSIM $GAME_PATH

d94fa8fd7d80a7d193276eebd849fb763c1707e2
MauriceElliott <mauricemelliott@gmail.com>
2026-01-27 23:17:11 +0000

    fixup! fixup! fixup! Finished off switch back

--- build.fish
+++ build.fish
@@ -1,11 +1,15 @@
 #!/usr/bin/env fish
 
-# Fixes an issue for Linux, or possibly specifically my gentoo install where when compiling, swift can't find my studio.h file.
-set -x ARM_NONE_EABI_GCC_PATH /usr/lib/gcc/arm-none-eabi/15
-set -x ARM_NONE_EABI_SYSROOT_PATH /usr/arm-none-eabi
-
+# Fixes an issue for Linux, or possibly specifically my gentoo install when when compiling, swift can't find my studio.h file.
+if test (hostname) = eighthCircle
+    set -x ARM_NONE_EABI_GCC_PATH /usr/lib/gcc/arm-none-eabi/15
+    set -x ARM_NONE_EABI_SYSROOT_PATH /usr/arm-none-eabi
+end
 set PRODUCT_NAME Gutwound
 set GAME_PATH $PLAYDATE_SDK_PATH/Disk/Games/$PRODUCT_NAME.pdx
+
+# Use the development snapshot toolchain for Embedded Swift armv7em target support
+set -x TOOLCHAINS swift
 
 swift package pdc 2>&1 | grep -v "libtinfo.so.6: no version information" | grep -v "libxml2.so.2: no version information" | grep -v "libcurl.so.4: no version information"
 
